ceacac5fc9f6745b8fd8c9e849071e34d08cac49
Wilian Rodrigues <wilian.rodrigues@saudegarantida.com.br>
2022-03-26 13:26:09 -0300

    first commit

---
+++ database/migrations/2022_03_26_155911_create_teachers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('email')->unique();
+            $table->string('cpf', 11)->unique();
+            $table->string('password');
+            $table->enum('discipline', [1,2,3]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
