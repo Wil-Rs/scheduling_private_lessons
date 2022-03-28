8659a374ece00e79fc110032d0aab8e8d7779973
Wilian Rodrigues <wilian.rodrigues@saudegarantida.com.br>
2022-03-27 21:33:31 -0300

    add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,5 +80,12 @@
             @yield('content')
         </main>
     </div>
+
+    <footer class="bg-light text-center text-lg-start fixed-bottom">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright:
+            <a class="text-dark" href="https://github.com/Wil-Rs">Wil</a>
+        </div>
+    </footer>
 </body>
 </html>
